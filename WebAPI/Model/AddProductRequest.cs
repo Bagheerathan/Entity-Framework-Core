d8289c08209593b1ea4a9218cd255ffc86513473
Bagheerathan <70306720+Bagheerathan@users.noreply.github.com>
2022-09-24 14:16:59 +0530

    Add project files.

---
+++ WebAPI/Model/AddProductRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Model
+{
+    public class AddProductRequest
+    {
+        public string ProductName { get; set; }
+        public string Availability { get; set; }
+        public string Brand { get; set; }
+
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ WebAPI/Model/UpdateProductRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Model
+{
+    public class UpdateProductRequest
+    {
+        public string ProductName { get; set; }
+        public string Brand { get; set; }
+        public string Availability { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
